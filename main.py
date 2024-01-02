cea18b2d94384e25a00738b585b11889c1a67253
Dawn Wu <njwzh_dawn@163.com>
2024-01-02 15:54:58 +0800

    fix: 指定保存到本地文件的编码为utf-8

--- main.py
+++ main.py
@@ -18,13 +18,13 @@ from visualization import by_matplotlib as matvis
 from Datanalysis import methodtime as mtime
 
 if __name__ == '__main__':
-    method_name = 'Create Subs For OE'
-    start_date = datetime.strptime('2023-09-30 00:00:00','%Y-%m-%d %H:%M:%S')
-    mtime.analyze_method_times(cloud_test=True, method_name=method_name, start_date=start_date)
+    # method_name = 'Create Subs For OE'
+    # start_date = datetime.strptime('2023-09-30 00:00:00','%Y-%m-%d %H:%M:%S')
+    # mtime.analyze_method_times(cloud_test=True, method_name=method_name, start_date=start_date)
     '''
     ## job_id
     TM: 2389
     UM: 67
     MTN: 2248, 2305
     '''
-    # mtime.analyze_serial_duration(job_id=67, date='2023-11-28', log=True)
+    mtime.analyze_serial_duration(job_id=2389, date='2023-12-26', log=True)
